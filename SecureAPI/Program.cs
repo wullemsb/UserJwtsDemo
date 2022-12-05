9b28b28710a235bf77497290b218a0f76422a750
Wullems Bart <wullemsb@gmail.com>
2022-12-05 08:58:23 +0100

    Update example

--- SecureAPI/Program.cs
+++ SecureAPI/Program.cs
@@ -1,6 +1,16 @@
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication("Bearer").AddJwtBearer();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseAuthorization();
+
+app.MapGet("/", () => "Hello, World!");
+app.MapGet("/secret", (ClaimsPrincipal user) => $"Hello {user.Identity?.Name}.")
+    .RequireAuthorization();
 
 app.Run();
